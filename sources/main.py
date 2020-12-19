66309741f8a62b8c7ab219681ea50498ba0d5cac
egoreli <veli4estva@gmail.com>
2020-12-19 15:59:55 +0400

    Код по работе с API вынесен в класс TinkoffApi, создана функция по преобразованию позиций в список для Excel

--- sources/main.py
+++ sources/main.py
@@ -1,31 +1,42 @@
-import os
 from pprint import pprint
+from decimal import Decimal
 
-from dotenv import load_dotenv
-import tinvest
+from tinkoffapi import TinkoffApi
 
-dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
-if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path=dotenv_path)
 
-TOKEN = os.getenv('TINKOFF_API_TOKEN')
-ACCOUNT_ID = os.getenv("TINKOFF_BROKER_ACCOUNT")
+def get_portfolio_positions_for_excel(positions, balance, usd_course):
+    """Преобразование позиций в удобный для Excel список"""
+    positions_rows = [('Название', 'Котировка', 'Цена, шт.', 'Кол-во', 'Общая цена, руб.', '%')]
 
-client = tinvest.SyncClient(TOKEN)
-api = tinvest.PortfolioApi(client)
+    for position in positions:
+        exceptions = ('Доллар США', 'Евро')
+        if position.name not in exceptions:
+            currency = position.average_position_price.currency.value
+            total_price = 0
 
-positions = api.portfolio_get(broker_account_id=ACCOUNT_ID).parse_json().payload.positions
-balance = api.portfolio_currencies_get(broker_account_id=ACCOUNT_ID).parse_json().payload.currencies[0]
+            if currency == 'RUB':
+                total_price = position.expected_yield.value + position.average_position_price.value \
+                              * position.balance
+            elif currency == 'USD':
+                total_price = round((position.expected_yield.value + position.average_position_price.value
+                                     * position.balance) * usd_course, 2)
 
-csv_rows = [';'.join(['Имя', 'Котировка', 'Количество', 'Цена', 'Валюта', 'Заработок'])]
-for position in positions:
-    csv_rows.append(';'.join(map(str, [position.name,
-                                       position.ticker,
-                                       position.balance,
-                                       position.average_position_price.value,
-                                       position.average_position_price.currency.value,
-                                       position.expected_yield.value])))
-csv_rows.append(';'.join(map(str, ['Баланс', balance.balance])))
+            positions_rows.append((position.name,
+                                   position.ticker,
+                                   round(position.average_position_price.value + position.expected_yield.value /
+                                         position.balance, 2),
+                                   int(position.balance),
+                                   total_price))
 
-with open('../Инвестиции.csv', 'w') as f:
-    f.write("\n".join(csv_rows))
+    positions_rows.append(('Баланс (RUB)', balance[0].balance))
+    positions_rows.append(('Баланс (USD)', balance[1].balance))
+
+    return positions_rows
+
+
+if __name__ == "__main__":
+    tinkoff = TinkoffApi()
+    positions, balance = tinkoff.get_portfolio_positions_and_balance()
+    portfolio = get_portfolio_positions_for_excel(positions, balance, tinkoff.get_usd_course())
+
+    pprint(portfolio)
---
+++ sources/tinkoffapi.py
@@ -0,0 +1,37 @@
+import os
+
+from dotenv import load_dotenv
+import tinvest
+
+
+class TinkoffApi:
+    """Обёртка для работы с API Тинькова на основе библиотеки tinvest"""
+
+    def __init__(self):
+        self.__init_env_var()
+        token = os.getenv('TINKOFF_API_TOKEN')
+        account_id = os.getenv("TINKOFF_BROKER_ACCOUNT")
+
+        self.__client = tinvest.SyncClient(token)
+        api = tinvest.PortfolioApi(self.__client)
+
+        self.__positions = api.portfolio_get(broker_account_id=account_id) \
+            .parse_json().payload.positions
+        self.__balance = api.portfolio_currencies_get(broker_account_id=account_id) \
+            .parse_json().payload.currencies
+
+    @staticmethod
+    def __init_env_var():
+        """Инициализация переменных окружения"""
+        dotenv_path = os.path.join(os.path.dirname(__file__), '..\.env')
+        if os.path.exists(dotenv_path):
+            load_dotenv(dotenv_path=dotenv_path)
+
+    def get_usd_course(self):
+        """Получение курса USD"""
+        return tinvest.MarketApi(self.__client).market_orderbook_get(figi="BBG0013HGFT4", depth=1) \
+            .parse_json().payload.last_price
+
+    def get_portfolio_positions_and_balance(self):
+        """Получение всех позиций портфеля и баланса в валюте"""
+        return self.__positions, self.__balance
